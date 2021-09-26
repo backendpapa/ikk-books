fae37b39efa08a3352d002c524f7f9290a6d6471
Akeju Paul <akejupaul10@gmail.com>
2021-09-27 00:57:34 +0100

    book ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,22 @@
 <template>
  <div>
-
+   <v-row class="mt-4" justify="center" align="center" no-gutters >
+     <v-spacer></v-spacer>
+     <v-text-field  filled  placeholder="Type book name or author" append-icon="mdi-magnify"   class="black--text text-caption font-weight-bold" @click:append="toggleMarker" solo flat color="none" background-color="white" ></v-text-field>
+  
+      <v-select
+      class="ml-4 text-caption font-weight-bold"
+          :items="select"
+          v-model="category"
+          placeholder="Category"
+        
+          solo
+          flat
+          filled
+          style="width:4rem;font-size:10px"
+        ></v-select>
+     <v-spacer></v-spacer>
+   </v-row>
  </div>
 </template>
 
@@ -12,11 +28,23 @@
     components: {
      
     },
+    data(){
+      return {
+        select:['Fictions','Tech','DIY','Science and Art'],
+        category:'Category'
+      }
+    },
+    methods:{
+      toggleMarker(){
+        // alert("searching")
+      }
+    }
   }
 </script>
 <style >
 ::-webkit-scrollbar {
   display: none;
 }
+
 
 </style>
