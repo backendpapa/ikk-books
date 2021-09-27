c6e2a0379e98261d7baabf0708767be5cbcf90ed
Akeju Paul <akejupaul10@gmail.com>
2021-09-27 11:18:18 +0100

    make it bettr

--- src/components/Home/LastAdded.vue
+++ src/components/Home/LastAdded.vue
@@ -1,12 +1,62 @@
 <template>
-  <div>
-      <p>Last added</p>
+  <div style="background-color:#F3F2F7">
+      
+    
+          <carousel class="d-none d-sm-flex" items="6" style="height:50vh">
+
+    <v-card flat v-for="i in 13" :key="i.i" width="200" height="300"  class="white pa-2">
+       <div>
+           <v-row no-gutters>
+               <v-spacer></v-spacer>
+              
+                   <p class="text-center"> <v-img width="170" src="https://placeimg.com/200/200/any?1"></v-img></p>
+                   
+               
+               <v-spacer></v-spacer>
+
+           </v-row>
+          <p style="font-size:14px" class=" ml-1 ">The sales Development Playbook</p>   
+          <p class="mt-n3 ml-1 font-weight-bold text-caption ">Allie Well</p> 
+          <p class="mt-n3 ml-1 font-weight-bold text-caption ">4,5<span class="grey--text">/5</span></p> 
+       </div>
+    </v-card>
+
+</carousel>
+
+<carousel  autoWidth class="d-flex d-sm-none animate__animated animate__fadeIn animate__slower" items="1" :nav="false"  style="height:50vh">
+
+    <v-card flat v-for="i in 13" :key="i.i" width="200" height="300"  class="white pa-2 ma-1">
+       <div>
+           <v-row no-gutters>
+               <v-spacer></v-spacer>
+              
+                   <p class="text-center"> <v-img width="170" src="https://placeimg.com/200/200/any?1"></v-img></p>
+                   
+               
+               <v-spacer></v-spacer>
+
+           </v-row>
+          <p style="font-size:14px" class=" ml-1 ">The camp Nou Revl</p>   
+          <p class="mt-n3 ml-1 font-weight-bold text-caption ">Allie Well</p> 
+          <p class="mt-n3 ml-1 font-weight-bold text-caption ">4,5<span class="grey--text">/5</span></p> 
+       </div>
+    </v-card>
+
+</carousel>
+     
+      
   </div>
 </template>
 
 <script>
+ import carousel from 'vue-owl-carousel'
 export default {
-
+    components: { carousel },
+    data(){
+        return {
+           
+    }
+}
 }
 </script>
 
--- src/components/Home/Tab.vue
+++ src/components/Home/Tab.vue
@@ -1,12 +1,12 @@
 <template>
   <div>
-       <v-tabs background-color="transparent"  v-model="tab" hide-slider >
+       <v-tabs class="animate__animated animate__fadeIn animate__slow" background-color="transparent"  v-model="tab" hide-slider >
         <v-tab v-for="item in items" class="text-caption font-weight-bold" style="text-transform:none" :key="item.tab">
           {{ item.tab }}
         </v-tab>
       </v-tabs>
       <v-tabs-items touchless v-model="tab"  >
-        <v-tab-item  v-for="item in items" :key="item.tab">
+        <v-tab-item class=""  v-for="item in items" :key="item.tab">
           <v-card color="" style="background-color:#F3F2F7" flat>
             <v-card-text>
               <component v-bind:is="item.content"></component>
